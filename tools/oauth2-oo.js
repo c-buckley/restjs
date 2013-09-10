f095b295c90b18cf46b4b1d1e9484cbb17ede793
Osmond <Kingozzy24@gmail.com>
2013-09-10 09:32:52 -0700

    oauth change

--- tools/oauth2-oo.js
+++ tools/oauth2-oo.js
@@ -15,15 +15,12 @@ function cloneObject(obj) {
 /**
 *	Oauth2
 *	handles Oauth2 calls according to spec
-*	@param	{object}	options		request options for refreshing
 *	@param	{object}	config		Oauth2 configs
 *	@constructor
 */
-function Oauth2(options, config) {
+function Oauth2(config) {
 	var self = this;
-	var optHolder = cloneObject(options);
 	var cHolder = cloneObject(config);
-	self.options = (optHolder !== -1) ? optHolder : {};
 	self.config = (cHolder !== -1) ? cHolder : {};
 	self.state = 0;
 	self.lastCheck = self.state;
@@ -38,6 +35,8 @@ Oauth2.prototype.refreshCall = function authCall(config) {
 	body.client_id = config.consumer_key;
 	body.refresh_token = config.refresh_token;
 	body = querystring.stringify(body);
+
+	head.hostname =  config.hostname || head.hostname;
 	head.method = 'POST';
 	head.headers['Content-Type'] = 'application/x-www-form-urlencoded';
 	head.path = config.refresh_path;
@@ -65,18 +64,19 @@ Oauth2.prototype.refreshToken = function refreshToken(auth, done) {
 		} else {
 			var newToken;
 			try {
-				newToken = JSON.parse(res.message);
+				newToken = JSON.parse(res.body);
 			} catch (e) {
+				console.log('parse error');
 				done(e);
 			}
-			if (Math.floor(newToken.statusCode / 100) !== 2) {
-				done(newToken.body);
+			if (Math.floor(res.statusCode / 100) !== 2) {
+				done(newToken);
 			} else {
 				self.state += 1;
 				auth.access_token = newToken.access_token;
 				if (newToken.expires_in || newToken.expires) {
 					var expires = newToken.expires_in || newToken.expires;
-					var time = new Date().getTime();
+					var time = Date.now();
 					expires = (expires * 1000) + time;
 					auth.expires = expires;
 				}
@@ -87,6 +87,8 @@ Oauth2.prototype.refreshToken = function refreshToken(auth, done) {
 			}
 		}
 	}
+	console.log(info.head);
+	console.log(info.body);
 	rest.request(info.head, info.body, tokenBack);
 };
 Oauth2.prototype.hasChanged = function () {
@@ -97,11 +99,10 @@ Oauth2.prototype.hasChanged = function () {
 	}
 	return false;
 };
-Oauth2.prototype.get = function () {
+Oauth2.prototype.getConfig = function () {
 	return cloneObject(this.config);
 };
-Oauth2.prototype.set = function (config) {
-	var self = this;
+Oauth2.prototype.setConfig = function (config) {
 	var newConfig = cloneObject(config);
 	if (newConfig === -1) {
 		return false;
@@ -109,6 +110,14 @@ Oauth2.prototype.set = function (config) {
 	this.config = newConfig;
 	return true;
 };
+Oauth2.prototype.setHead = function (options) {
+	var newOptions = cloneObject(options);
+	if (newOptions === -1) {
+		return false;
+	}
+	this.options = newOptions;
+	return true;
+}
 Oauth2.prototype.head = function () {
 	return this.options;
 };
@@ -116,24 +125,16 @@ Oauth2.prototype.head = function () {
 /**
 *	call
 *	attempts to make specified call, refreshes token on failuer
-*	@param	{object}	options		call options
 *	@param	{string}	body			body of request
-*	@param	{object|function}	[auth]		optional auth object if override is wanted, pass in function if not needed
 *	@param	{function}	callback	function to call on completion
 */
-Oauth2.prototype.call = function attemptRequest(options, body, auth, callback) {
+Oauth2.prototype.call = function attemptRequest(options, body, callback) {
 	var self = this;
 	var currentTime = new Date().getTime();
-	
-	if(typeof(auth) !== "function"){
-		auth = (auth) ? auth : this.config;
-	} else{
-		callback = auth;
-	}
-	
-	if (auth.expires <= (currentTime - 172800000)) {
+
+	if (self.config.expires <= (currentTime - 172800000)) {
 		console.log('old token');
-		self.refreshToken(auth, function (err, newAuth) {
+		self.refreshToken(self.config, function (err, newAuth) {
 			if (err) {
 				return callback(err);
 			}
@@ -144,9 +145,10 @@ Oauth2.prototype.call = function attemptRequest(options, body, auth, callback) {
 			if (err) {
 				callback(err);
 			} else {
+				console.log(response.statusCode);
 				if (response.statusCode === 401) {
 					console.log(response.body);
-					self.refreshToken(auth, function (err, newAuth) {
+					self.refreshToken(self.config, function (err, newAuth) {
 						if (err) {
 							return callback(err);
 						}
